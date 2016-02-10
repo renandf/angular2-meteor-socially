744d3be34e5ce81fcac715d0530994218e606f24
Renan <renandf@gmail.com>
2016-02-10 16:33:41 +1100

    first commit

---
+++ client/parties-list/parties-list.ts
@@ -0,0 +1,26 @@
+import {Component, View} from 'angular2/core';
+
+import {Parties} from 'collections/parties';
+
+import {PartiesForm} from 'client/parties-form/parties-form';
+
+import {RouterLink} from 'angular2/router';
+
+@Component({
+  selector: 'parties-list'
+})
+@View({
+  templateUrl: '/client/parties-list/parties-list.html',
+  directives: [PartiesForm, RouterLink]
+})
+export class PartiesList {
+  parties: Mongo.Cursor<Object>;
+
+  constructor() {
+    this.parties = Parties.find();
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+}
---
+++ client/party-details/party-details.ts
@@ -0,0 +1,21 @@
+import {Component, View} from 'angular2/core';
+
+import {RouteParams, RouterLink} from 'angular2/router';
+
+import {Parties} from 'collections/parties';
+
+@Component({
+  selector: 'party-details'
+})
+@View({
+  templateUrl: '/client/party-details/party-details.html',
+  directives: [RouterLink]
+})
+export class PartyDetails {
+  party: Object;
+
+  constructor(params: RouteParams) {
+    var partyId = params.get('partyId');
+    this.party = Parties.findOne(partyId);
+  }
+}
